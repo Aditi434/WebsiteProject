0b9f478ce379a2d2226b2362471b1be2c6696bf4
Aditi434 <82752546+Aditi434@users.noreply.github.com>
2021-05-12 10:41:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var line="Welcome to my first console project";
+console.log(line);
+
+var firstName = "Aditi";
+var lastName= "Chelamchela";
+
+var full_name=firstName.concat(" ",lastName);
+console.log(full_name);
+
+var a = 44;
+var b = 33;
+var add= a+b; 
+console.log(add);
+
+var sub= a-b;
+console.log(sub);
+
+console.log(Math.min(a,b));
+console.log(Math.max(a,b));
+
